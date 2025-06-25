daecefaaf00b049688f47b19fb8610ba3b69db29
Asser Hakala <extern.asser.hakala@digg.se>
2025-06-25 17:24:30 +0200

    feat: add basic presentation deeplink and route
    Signed-off-by: Asser Hakala <extern.asser.hakala@digg.se>

--- Wallet/Core/Navigation/Route.swift
+++ Wallet/Core/Navigation/Route.swift
@@ -1,5 +1,7 @@
 import Foundation
+import SiopOpenID4VP
 
 enum Route: Hashable, Codable {
+  case presentation(definition: PresentationDefinition)
   case issuance(credentialOfferUri: String)
 }
--- Wallet/Core/Networking/NetworkClient.swift
+++ Wallet/Core/Networking/NetworkClient.swift
@@ -3,6 +3,13 @@ import Foundation
 enum HTTPMethod: String {
   case get = "GET"
   case post = "POST"
+  
+  init?(from string: String?) {
+    guard let string else {
+      return nil
+    }
+    self.init(rawValue: string.uppercased())
+  }
 }
 
 final class NetworkClient {
@@ -13,17 +20,23 @@ final class NetworkClient {
   
   private init() {}
   
-  func fetch<T: Decodable>(
+  private func performRequest(
     _ url: URL,
     method: HTTPMethod = .get,
+    contentType: String? = nil,
+    accept: String? = nil,
     token: String? = nil,
     body: (any Encodable)? = nil
-  ) async throws -> T {
+  ) async throws -> Data {
     var request = URLRequest(url: url)
     request.httpMethod = method.rawValue
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.setValue("application/json", forHTTPHeaderField: "Accept")
-
+    
+    if let contentType {
+      request.setValue(contentType, forHTTPHeaderField: "Content-Type")
+    }
+    if let accept {
+      request.setValue(accept, forHTTPHeaderField: "Accept")
+    }
     if let token {
       request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
     }
@@ -61,6 +74,35 @@ final class NetworkClient {
     default:
       throw HTTPError.serverError(httpResponse.statusCode)
     }
+    
+    return data
+  }
+  
+  func fetchData(
+    _ url: URL,
+    method: HTTPMethod = .get,
+    contentType: String? = nil,
+    accept: String? = nil,
+    token: String? = nil,
+    body: (any Encodable)? = nil
+  ) async throws -> Data {
+    return try await performRequest(url, method: method, contentType: contentType, accept: accept, token: token, body: body)
+  }
+  
+  func fetch<T: Decodable>(
+    _ url: URL,
+    method: HTTPMethod = .get,
+    token: String? = nil,
+    body: (any Encodable)? = nil
+  ) async throws -> T {
+    let data = try await performRequest(
+      url,
+      method: method,
+      contentType: "application/json",
+      accept: "application/json",
+      token: token,
+      body: body
+    )
     
     do {
       return try decoder.decode(T.self, from: data)
