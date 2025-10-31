5c5124714c8ac8b1a08c1c042b38a433de05b579
Asser Hakala <extern.asser.hakala@digg.se>
2025-10-31 10:55:27 +0100

    feat: add support for Wallet Unit Attestations (#14)
    * feat: add OpenAPI code generator and integrate with BFF (Client
      Gateway)
    
    Signed-off-by: Asser Hakala <extern.asser.hakala@digg.se>

--- Wallet/WalletApp.swift
+++ Wallet/WalletApp.swift
@@ -1,12 +1,16 @@
 import SwiftData
 import SwiftUI
+import WalletMacrosClient
 
 @main
 struct WalletApp: App {
+  let clientGateway = GatewayClient()
+
   var body: some Scene {
     WindowGroup {
       AppRootView()
         .modelContainer(for: AppSession.self)
+        .environment(\.gatewayClient, clientGateway)
     }
   }
 }
