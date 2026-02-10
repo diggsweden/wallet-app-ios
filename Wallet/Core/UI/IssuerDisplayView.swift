7eb954a528609813c22ca9b42b3bd61c3d7eaebc
Asser Hakala <extern.asser.hakala@digg.se>
2026-02-10 12:53:22 +0100

    fix: issuer display image respects bounds
    Signed-off-by: Asser Hakala <extern.asser.hakala@digg.se>

--- Wallet/Core/UI/IssuerDisplayView.swift
+++ Wallet/Core/UI/IssuerDisplayView.swift
@@ -1,14 +1,22 @@
 import OpenID4VCI
 import SwiftUI
+import WalletMacrosClient
 
 struct IssuerDisplayView: View {
   let issuerDisplayData: IssuerDisplay
 
   var body: some View {
     VStack(spacing: 12) {
-      AsyncImage(url: issuerDisplayData.imageUrl)
-        .frame(maxWidth: .infinity, maxHeight: 200, alignment: .center)
-        .padding(.bottom, 10)
+      AsyncImage(url: issuerDisplayData.imageUrl) { image in
+        image
+          .resizable()
+          .scaledToFit()
+          .frame(maxHeight: 200)
+      } placeholder: {
+        ProgressView()
+      }
+      .fixedSize(horizontal: false, vertical: true)
+      .padding(.bottom, 10)
 
       VStack(spacing: 5) {
         Text("Utfärdare:").textStyle(.h3)
@@ -17,4 +25,14 @@ struct IssuerDisplayView: View {
     }
     .frame(maxWidth: .infinity)
   }
+}
+
+#Preview {
+  IssuerDisplayView(
+    issuerDisplayData: IssuerDisplay(
+      name: "Test",
+      info: nil,
+      imageUrl: #URL("https://dela.digg.se/img/sr-logo-color.png")
+    )
+  )
 }
