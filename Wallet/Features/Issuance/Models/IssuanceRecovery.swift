a2dec3cbd5e55cb28655ad26f25134a40b0f5414
David <david.mansourian@digg.se>
2026-09-23 10:38:13 +0200

    chore: remove unused issuance recovery file
    Signed-off-by: David <david.mansourian@digg.se>

--- Wallet/Features/Issuance/Models/IssuanceRecovery.swift
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2026 Digg - Agency for digital government
-//
-// SPDX-License-Identifier: EUPL-1.2
-
-enum IssuanceRecovery {
-  case start
-  case authorize
-  case fetchCredential
-}
